11ee36a03c09c3a06ba643505491a496ad47cf1d
Dabemuc <daniel.benner02@gmail.com>
2025-09-26 11:51:14 +0200

    build fix

--- src/db/cache.ts
+++ src/db/cache.ts
@@ -4,37 +4,37 @@ import { upstashCache } from "drizzle-orm/cache/upstash";
 async function isReachable(url: string): Promise<boolean> {
   try {
     const res = await fetch(url, {
-      method: "HEAD",
       signal: AbortSignal.timeout(2000),
     });
+    console.log(res);
     return res.ok;
   } catch {
     return false;
   }
 }
 
-export async function getCacheConf() {
+export function getCacheConf() {
   let cacheConfig;
 
   if (process.env.NODE_ENV === "development") {
     logger.info("Checking localdeployment cache availability...");
 
     const localUrl = "http://localhost:8079";
-    const reachable = await isReachable(localUrl);
-
-    if (reachable) {
-      logger.info("Utilizing localdeployment cache ...");
-      cacheConfig = upstashCache({
-        url: localUrl,
-        token: "example_token",
-        global: true,
-        config: {
-          ex: 60 * 60 * 24, // 24h TTL
-        },
-      });
-    } else {
-      logger.warn("Local cache not reachable. Skipping!");
-    }
+    isReachable(localUrl).then((reachable) => {
+      if (reachable) {
+        logger.info("Utilizing localdeployment cache ...");
+        cacheConfig = upstashCache({
+          url: localUrl,
+          token: "example_token",
+          global: true,
+          config: {
+            ex: 60 * 60 * 24, // 24h TTL
+          },
+        });
+      } else {
+        logger.warn("Local cache not reachable. Skipping!");
+      }
+    });
   } else if (
     process.env.UPSTASH_REDIS_REST_URL &&
     process.env.UPSTASH_REDIS_REST_TOKEN
